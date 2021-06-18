589533463fd3534ec3a16495e45286d6cdc8cbc7
Nathan <nathan.rendall@gmail.com>
2021-06-18 14:15:21 +0100

    function 3 added - passes both add and remove cash tests

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -8,3 +8,6 @@ def get_pet_shop_name(given_dict):
 def get_total_cash(given_dict):
     return given_dict["admin"]["total_cash"]
 
+# value of cash will be added to total_cash. 
+def add_or_remove_cash(given_dict, cash):
+    given_dict["admin"]["total_cash"] += cash
