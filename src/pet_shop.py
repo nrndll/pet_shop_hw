228be42ac4655756cda1f30d338ee7a4a92f905e
Nathan <nathan.rendall@gmail.com>
2021-06-18 14:01:42 +0100

    function 2 added

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,6 +1,10 @@
 # WRITE YOUR FUNCTIONS HERE
 
-# given a dictionary, this will return the value associated with "name"
+# given a dict, this will return the value at "name"
 def get_pet_shop_name(given_dict):
     return given_dict["name"]
+
+# given a dict, this will return the value within "admin" dict at "total_cash"
+def get_total_cash(given_dict):
+    return given_dict["admin"]["total_cash"]
 
