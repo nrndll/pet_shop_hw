c8cea21a655d601d852bef3ce8b5adf25108ad30
Nathan <nathan.rendall@gmail.com>
2021-06-18 15:07:23 +0100

    function 7 added

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -23,3 +23,13 @@ def increase_pets_sold(given_dict, number_of_pets_sold):
 # get number of items from the "pets" list, i.e. the total stock of the petshop.
 def get_stock_count(given_dict):
     return len(given_dict["pets"])
+
+# for each item in "pets" list, compare "breed" with "given_breed" parameter and if they are eqaul, add it to "pets_of_given_breed" list.
+def get_pets_by_breed(given_dict, given_breed):
+    # total_pets_by_breed = 0
+    pets_of_given_breed = []
+    for pet in given_dict["pets"]:
+        if pet["breed"] == given_breed:
+    #         total_pets_by_breed += 1
+            pets_of_given_breed.append(pet)
+    return pets_of_given_breed
